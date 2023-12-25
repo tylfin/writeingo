b75a33c654dfbd079f7b2f9eda8c6d8cae5baf26
Tyler <tylfin@gmail.com>
2023-12-25 09:39:30 -0500

    Add IfElse and error support

--- object/object.go
+++ object/object.go
@@ -5,9 +5,11 @@ import "fmt"
 type ObjectType string
 
 const (
-	NULL_OBJ    ObjectType = "NULL"
-	INTEGER_OBJ ObjectType = "INTEGER"
-	BOOLEAN_OBJ ObjectType = "BOOLEAN"
+	NULL_OBJ         ObjectType = "NULL"
+	INTEGER_OBJ      ObjectType = "INTEGER"
+	BOOLEAN_OBJ      ObjectType = "BOOLEAN"
+	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
+	ERROR_OBJ        ObjectType = "ERROR"
 )
 
 type Object interface {
@@ -33,3 +35,17 @@ type Boolean struct {
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return fmt.Sprintf("ERROR: %s", e.Message) }
