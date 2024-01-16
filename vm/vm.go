9706be4a839894b9c17c87b6d4c37053287df9db
Tyler <tylfin@gmail.com>
2024-01-16 10:14:57 -0500

    Feat: Add subtraction, mult, div to the compiler/VM

--- vm/vm.go
+++ vm/vm.go
@@ -38,17 +38,20 @@ func (vm *VM) Run() error {
 			if err := vm.push(vm.constants[constIndex]); err != nil {
 				return err
 			}
-		case code.OpAdd:
-			left := vm.pop()
-			right := vm.pop()
-			leftValue := left.(*object.Integer).Value
-			rightValue := right.(*object.Integer).Value
-
-			result := leftValue + rightValue
-			if err := vm.push(&object.Integer{Value: result}); err != nil {
+		case code.OpAdd, code.OpSub, code.OpMul, code.OpDiv:
+			if err := vm.executeBinaryOperation(op); err != nil {
 				return err
-
 			}
+			// left := vm.pop()
+			// right := vm.pop()
+			// leftValue := left.(*object.Integer).Value
+			// rightValue := right.(*object.Integer).Value
+
+			// result := leftValue + rightValue
+			// if err := vm.push(&object.Integer{Value: result}); err != nil {
+			// 	return err
+
+			// }
 		case code.OpPop:
 			vm.pop()
 		}
@@ -59,6 +62,39 @@ func (vm *VM) Run() error {
 
 func (vm *VM) LastPoppedStackElem() object.Object {
 	return vm.stack[vm.sp]
+}
+
+func (vm *VM) executeBinaryOperation(op code.Opcode) error {
+	right := vm.pop()
+	left := vm.pop()
+	leftType := left.Type()
+	rightType := right.Type()
+
+	if leftType == object.INTEGER_OBJ && rightType == object.INTEGER_OBJ {
+		return vm.executeBinaryIntegerOperation(op, left, right)
+	}
+	return fmt.Errorf("unsupported types for binary operation: %s %s", leftType, rightType)
+}
+
+func (vm *VM) executeBinaryIntegerOperation(op code.Opcode, left, right object.Object) error {
+	var result int64
+
+	leftValue := left.(*object.Integer).Value
+	rightValue := right.(*object.Integer).Value
+	switch op {
+	case code.OpAdd:
+		result = leftValue + rightValue
+	case code.OpSub:
+		result = leftValue - rightValue
+	case code.OpMul:
+		result = leftValue * rightValue
+	case code.OpDiv:
+		result = leftValue / rightValue
+	default:
+		return fmt.Errorf("unknown integer operator: %d", op)
+	}
+
+	return vm.push(&object.Integer{Value: result})
 }
 
 func (vm *VM) push(o object.Object) error {
